f2edb2b83b393cdab5a860b9d37e80118d4f5ffb
vicchen0321 <victor72441299@gmail.com>
2024-04-05 15:27:31 +0800

    Init commit

---
+++ src/main/java/com/asiayo/api/AsiaYoApplication.java
@@ -0,0 +1,13 @@
+package com.asiayo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiaYoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AsiaYoApplication.class, args);
+  }
+
+}
